cfa390eb6a2ce734d43f8a47f395b69d9de3ac33
João Araujo <joaomigueldacostaaraujo@gmail.com>
2024-11-01 04:24:56 -0300

    update views.py and urls.py

--- pegaai/app/urls.py
+++ pegaai/app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home),
+    path("registrar", views.register),
+    path("entrar", views.login),
+    path("menu", views.menu),
+    path("pedidos", views.orders),
+    path("perfil", views.profile)
 ]
