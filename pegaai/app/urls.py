54af5c60fb31466628b087e5fc1b889fd2f32b4b
João Araujo <joaomigueldacostaaraujo@gmail.com>
2024-11-05 04:15:41 -0300

    Acabando de transformar todos os CSSs para TailWind

--- pegaai/app/urls.py
+++ pegaai/app/urls.py
@@ -4,8 +4,12 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("registrar", views.register),
+    path("register", views.register),
     path("entrar", views.login),
+    path("login", views.login),
     path("menu", views.menu),
     path("pedidos", views.orders, name="orders"),
-    path("perfil", views.profile, name="profile")
+    path("orders", views.orders, name="orders"),
+    path("perfil", views.profile, name="profile"),
+    path("profile", views.profile, name="profile")
 ]
