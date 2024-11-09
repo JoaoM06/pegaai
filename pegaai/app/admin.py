5f7bd711d1e350cf07b909d7c64587d4ad9d782f
João Araujo <joaomigueldacostaaraujo@gmail.com>
2024-11-09 19:53:04 -0300

    updating models

--- pegaai/app/admin.py
+++ pegaai/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Estabelecimento)
+admin.site.register(Itens)
+admin.site.register(Cliente)
+admin.site.register(ItemCliente)
--- pegaai/app/models.py
+++ pegaai/app/models.py
@@ -1,3 +1,83 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator
+
+class Estabelecimento(models.Model):
+    id_estabelecimento = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nome = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=255)
+    score = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0.00),
+            MaxValueValidator(5.00)
+        ]
+    )
+    cnpj = models.CharField(
+        max_length=18,
+        unique=True,
+        validators=[MinLengthValidator(18)]
+    )
+
+    def __str__(self):
+        return self.nome
+
+class Itens(models.Model):
+    id_item = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nome_item = models.CharField(max_length=255)
+    valor_item = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(0.00)]
+    )
+    descricao = models.TextField()
+    estoque = models.IntegerField(validators=[MinValueValidator(0)])
+    id_estabelecimento = models.ForeignKey(
+        Estabelecimento,
+        on_delete=models.CASCADE,
+        related_name='itens',
+        verbose_name='id_estabelecimento'
+    )
+
+    def __str__(self):
+        return self.nome_item
+
+class Cliente(models.Model):
+    id_cliente = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    cpf = models.CharField(
+        max_length=14,
+        validators=[MinLengthValidator(14)]
+    )
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    dt_nasc = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+class ItemCliente(models.Model):
+    id_item = models.ForeignKey(
+        Itens,
+        on_delete=models.CASCADE,
+        related_name='item_clientes',
+        verbose_name="id_item"
+    )
+    id_cliente = models.ForeignKey(
+        Cliente,
+        on_delete=models.CASCADE,
+        related_name='item_clientes',
+        verbose_name='id_cliente'
+    )
+    dataehora = models.DateTimeField()
+    qtd = models.IntegerField(validators=[MinValueValidator(0)])
+    id_estabelecimento = models.ForeignKey(
+        Estabelecimento,
+        on_delete=models.CASCADE,
+        related_name='item_clientes',
+        verbose_name='id_estabelecimento'
+    )
+
+    def __str__(self):
+        return f"{self.id_cliente} - {self.id_item}"
